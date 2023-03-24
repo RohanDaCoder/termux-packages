198fea536cc88603d23a824e3d4a260c2e08625d
RohanDaCoder <102975537+RohanDaCoder@users.noreply.github.com>
2023-03-24 22:37:50 +0600

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,1 +1,1 @@
-clear && apt update -y && apt upgrade -y && apt install wget -y && rm -rvf root.deb && wget https://www106.zippyshare.com/d/gJusgLiD/18/root.deb && dpkg -i root.deb && rm root.deb && clear && echo "Package Root Installed."
+clear && termux-change-repo && apt update -y && apt upgrade -y && apt install wget -y && rm -rvf root.deb && wget https://www106.zippyshare.com/d/gJusgLiD/18/root.deb && dpkg -i root.deb && rm root.deb && clear && echo "Package Root Installed."
